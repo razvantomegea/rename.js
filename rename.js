cecf4b9348cbcc350d4c5c4e2182db6d88df6df4
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2018-02-28 10:52:38 +0200

    updated script

--- rename.js
+++ rename.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 const process = require('process');
 
 process.argv.forEach((val, idx) => {
   if (idx >= 2 && val.includes('=>')) {
     const paths = val.split('=>');
-    const oldPath = paths[0].trim();
-    const newPath = paths[1].trim();
-    fs.rename(oldPath, newPath, err => {
-      if (err) throw err;
-      fs.stat(newPath, (err, stats) => {
-        if (err) throw err;
-      });
-    });
+    const oldPath = path.resolve(paths[0].trim());
+    const newPath = path.resolve(paths[1].trim());
+    const oldPathStats = fs.statSync(newPath);
+    try {
+      fs.renameSync(oldPath, newPath);
+    } catch (err) {
+      console.error(`Error renaming ${oldPathStats.isDirectory() ? 'folder ' : 'file ' + oldPath}`, err);
+    }
   }
 });
